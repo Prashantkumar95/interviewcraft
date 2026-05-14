8cdb62e750ec8544944710808363a31cc44cb9d9
Prashant Kumar <prashant63957174@gmail.com>
2026-05-14 21:22:16 +0530

    frontend connect

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewcraft-y8qp.onrender.com",
     credentials:true
 }))
 
